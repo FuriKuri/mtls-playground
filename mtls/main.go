4a92cef0a1d473636b02f792e33ef139faca3a63
Theo Pack <tf.pack@gmail.com>
2020-09-06 16:08:48 +0000

    Add mtls example

---
+++ mtls/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, world!\n")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	caCert, err := ioutil.ReadFile("../certs/rootCA.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	tlsConfig := &tls.Config{
+		ClientCAs: caCertPool,
+		ClientAuth: tls.RequireAndVerifyClientCert,
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	server := &http.Server{
+		Addr:      ":8443",
+		TLSConfig: tlsConfig,
+	}
+
+	log.Fatal(server.ListenAndServeTLS("../certs/mtls.furikuri.net.crt", "../certs/mtls.furikuri.net.key"))
+}
